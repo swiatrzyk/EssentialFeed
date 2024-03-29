20ef50fcf94fec2fe94731b9d04c607b6cf866a5
Sebastian Wiatrzyk <sebastian.wiatrzyk@gmail.com>
2024-03-29 19:22:23 +0100

    Create FeedItem

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  FeedItem
+//
+//  Created by Sebastian Wiatrzyk on 29/03/2024.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
