71dbff7dbfa24deee42b73a6b99a4fd793dd2192
Sebastian Wiatrzyk <sebastian.wiatrzyk@gmail.com>
2024-05-16 16:13:24 +0200

    Create URLSession data task with URL on `URLSessionHTTPClient.get(from: url)`

--- EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
@@ -8,7 +8,6 @@
 import XCTest
 import EssentialFeed
 
-
 final class RemoteFeedLoaderTests: XCTestCase {
     func test_init_doesNotRequestDataFromURL() {
         let (_, client) = makeSUT()
---
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -0,0 +1,48 @@
+//
+//  URLSessionHTTPClientTests.swift
+//  EssentialFeedTests
+//
+//  Created by Sebastian Wiatrzyk on 16/05/2024.
+//
+
+import XCTest
+
+class URLSessionHTTPClient {
+    private let session: URLSession
+
+    init(session: URLSession) {
+        self.session = session
+    }
+
+    func get(from url: URL) {
+        session.dataTask(with: url) { _, _, _ in }
+    }
+}
+
+final class URLSessionHTTPClientTests: XCTestCase {
+
+    func test_getFromURL_createsDataTaskWithURL() {
+        let url = URL(string: "http://a-url.com")!
+        let session = URLSessionSpy()
+        let sut = URLSessionHTTPClient(session: session)
+
+        sut.get(from: url)
+
+        XCTAssertEqual(session.receivedURLs, [url])
+    }
+
+    // MARK: - Helpers
+    
+    private class URLSessionSpy: URLSession {
+        var receivedURLs = [URL]()
+
+        override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, (any Error)?) -> Void) -> URLSessionDataTask {
+            receivedURLs.append(url)
+
+            return FakeURLSessionDataTask()
+        }
+    }
+
+
+    private class FakeURLSessionDataTask: URLSessionDataTask {}
+}
