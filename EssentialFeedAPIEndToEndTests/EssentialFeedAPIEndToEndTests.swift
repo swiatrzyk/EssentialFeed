62d3fe08aaccfcfdbe6ec3f2597bb8f6fdec7443
Sebastian Wiatrzyk <sebastian.wiatrzyk@gmail.com>
2024-06-05 19:17:49 +0200

    Create Github Actions CI workflow

---
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -0,0 +1,16 @@
+//
+//  EssentialFeedAPIEndToEndTests.swift
+//  EssentialFeedAPIEndToEndTests
+//
+//  Created by Sebastian Wiatrzyk on 03/06/2024.
+//
+
+import XCTest
+
+final class EssentialFeedAPIEndToEndTests: XCTestCase {
+
+    func test_endToEndTestServerGETFeedResult_matchesFixedTestAccountData() {
+//        let loader = RemoteFeedLoader(url: <#T##URL#>, client: <#T##HTTPClient#>)
+    }
+
+}
